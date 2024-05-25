a852275a1e49a8b4e7dd558652067a33196831c2
benard <benardboiwo703@gmail.com>
2024-05-25 11:14:20 +0300

    initial commit

--- lib/testing/dog_test.py
+++ lib/testing/dog_test.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env python3
 
-from dog import Dog
+from dog import dog
 
 class TestDog:
     '''Dog in dog.py'''
 
     def test_is_class(self):
         '''is a class with the name "Dog"'''
-        fido = Dog("Fido")
-        assert(type(fido) == Dog)
+        fido = dog("Fido")
+        assert(type(fido) == dog)
 
 class TestInit:
     '''Dog.__init__ in dog.py'''
 
     def test_saves_self_name(self):
         '''takes a name as an argument and saves it to self.name'''
-        fido = Dog("Fido")
+        fido = dog("Fido")
         assert(fido.name == "Fido")
 
     def test_saves_self_breed(self):
         '''takes a breed as an argument and saves it to self.breed'''
-        fido = Dog("Fido", "Dalmatian")
+        fido = dog("Fido", "Dalmatian")
         assert(fido.breed == "Dalmatian")
 
     def test_default_breed(self):
         '''sets self.breed = "Mutt" when no breed specified'''
-        fido = Dog("Fido")
+        fido = dog("Fido")
         assert(fido.breed == "Mutt")
